716b53d290bd64871985dcda7ab6bf28f6d0934a
Shreeti Das <shreetireni27@gmail.com>
2026-03-25 13:24:37 +0530

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,7 +2,15 @@ public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+
+        }
 
     }
 
