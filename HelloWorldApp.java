9197f9d29e642402b8c038ecd1d2a99454442f6a
Shreeti Das <shreetireni27@gmail.com>
2026-03-25 13:26:32 +0530

    Update HelloWorldApp.java

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,7 +2,21 @@ public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            String result = "";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // Remove trailing ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello, " + result + "!");
+        }
 
     }
 
